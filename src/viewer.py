fdd8b960efd4eeb747ac34c65b3c02f3b63b6383
Daniel F Moisset <dfmoisset@gmail.com>
2024-05-20 16:57:48 -0400

    Handle multiline tokens

--- src/viewer.py
+++ src/viewer.py
@@ -85,17 +85,20 @@ class TokenWidget(ScrollableContainer):
 
     def format_token(
         self, token: tokenize.TokenInfo, current_line: int
-    ) -> tuple[str, int]:
+    ) -> tuple[str, int, int]:
         line = token.start[0]
-        if line != current_line:
-            line_marker = "%4d: " % line
+        end_line = token.end[0]
+        if end_line != line:
+            line_marker = f"{line:4d}-{end_line}: "
+        elif line != current_line:
+            line_marker = f"{line:4d}: "
         else:
             line_marker = "      "
 
         return (
             line_marker
             + f"{tok_name[token.exact_type]:10} {token.string!r} start={token.start} end={token.end}"
-        ), line
+        ), line, end_line+1
 
     def update(self, tokens: Iterable[tokenize.TokenInfo]) -> None:
         token_lines: list[str] = []
@@ -104,8 +107,9 @@ class TokenWidget(ScrollableContainer):
         width = 0
         current_line = 0
         for token_idx, token in enumerate(tokens):
-            formatted_token, current_line = self.format_token(token, current_line)
-            self.lineno_map[current_line].add(token_idx + 1)
+            formatted_token, current_line, token_end_line = self.format_token(token, current_line)
+            for lineno in range(current_line, token_end_line):
+                self.lineno_map[lineno].add(token_idx + 1)
             self.token_positions.append(current_line)
             token_lines.append(formatted_token)
             width = max(width, len(formatted_token))
